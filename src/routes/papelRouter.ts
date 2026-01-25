cb3c22e363e37ddfa2d083f2df7d059e7eebc63d
Mazzega_Dev <=>
2026-01-25 15:11:26 -0300

    minor changes

--- src/routes/papelRouter.ts
+++ src/routes/papelRouter.ts
@@ -7,8 +7,8 @@ const router: Router = Router();
 const controller: PapelController = new PapelController();
 
 router.get("/funcoes", validateAuthAdm,(req: Request, res: Response) => {
-   // #swagger.tags = ['Papel']
-   // #swagger.summary = 'Lista todos os papeis no sistema'
+   // #swagger.tags = ['Cargo']
+   // #swagger.summary = 'Lista todos os cargos no sistema'
    /* #swagger.security = [{
         "bearerAuth": []
     }]
